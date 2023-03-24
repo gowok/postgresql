7ecd6b1c71a41a51eee56a1b26f531517c6940ae
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2023-03-24 14:11:30 +0700

    🚀 launched

---
+++ postgres.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/gowok/gowok/config"
+	"github.com/gowok/gowok/driver/database"
+	_ "github.com/lib/pq"
+)
+
+type PostgreSQL struct {
+	*sql.DB
+}
+
+var _ database.SQLExecutor = PostgreSQL{}
+var _ database.SQLQuerier = PostgreSQL{}
+var _ database.SQLPreparation = PostgreSQL{}
+
+func New(conf config.Database) (*PostgreSQL, error) {
+	db, err := sql.Open("postgres", conf.DSN)
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostgreSQL{db}, nil
+}
